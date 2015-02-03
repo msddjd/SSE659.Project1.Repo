3bc0ecaccdf82949a074fa5229d5d4f7aaaec1b1
msddjd <mike.sdj.dumas@gmail.com>
2015-02-03 13:09:06 -0500

    Added a line to my branch

--- SSE659.Project1/src/testing.java
+++ SSE659.Project1/src/testing.java
@@ -3,7 +3,8 @@ public class testing {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Testing 123");		
+		System.out.println("Testing 123");
+		System.out.println("This is just my branch");
 
 	}
 
