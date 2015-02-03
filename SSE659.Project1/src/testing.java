ab33e0ce3ebbef164858df458e6a135386e4bee5
msddjd <mike.sdj.dumas@gmail.com>
2015-02-03 13:02:00 -0500

    Mike added a line of code

--- SSE659.Project1/src/testing.java
+++ SSE659.Project1/src/testing.java
@@ -4,6 +4,7 @@ public class testing {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Testing 123");
+		System.out.println("Mike made a change");
 
 	}
 
