7e30440bcc8ce1ddc8b5f75cdb16c56cd7c576ba
msddjd <mike.sdj.dumas@gmail.com>
2015-02-10 19:30:05 -0500

    Test Case (g != i) Successfully Tested & Implemented

--- SSE659.TDDnDVCS/src/EngEconomyCalc.java
+++ SSE659.TDDnDVCS/src/EngEconomyCalc.java
@@ -1,10 +1,9 @@
 public class EngEconomyCalc {
 			
 	double findPgGivenA(double A, double g, double i, int n){
-				
-		return (A*(n/(1+i/100.00)));		
-		
-	}
+		if(g==i)		
+		return (A*(n/(1+i/100.00)));
+		else
+		return A*(1 - Math.pow( ((1+g)/(1+i)), n ))/(i-g);
+	}	
 }
-
-//return Math.round( (A*(n/(1+i/100.00)))*10000.0/10000.0 );
--- SSE659.TDDnDVCS/src/PgATest.java
+++ SSE659.TDDnDVCS/src/PgATest.java
@@ -2,7 +2,6 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-
 public class PgATest {
 
 	@Test
@@ -11,6 +10,15 @@ public class PgATest {
 		double A = 2800000, g = 8.0, i = 8.0;
 		int n = 6;
 		double correct_answer = 15555555.56;//Found via calculator
+		assertEquals(mycalc.findPgGivenA(A,g,i,n),correct_answer,0.01);
+	}
+	
+	@Test
+	public void test_g_noteq_i(){
+		EngEconomyCalc mycalc = new EngEconomyCalc();
+		double A = 2800000, g = 8.0, i = 7.0;
+		int n = 6;
+		double correct_answer = 2876402.283;
 		assertEquals(mycalc.findPgGivenA(A,g,i,n),correct_answer,0.01);
 	}
 
