561cb2b33ff0f2b52a5857c11cc5108947bbac80
msddjd <mike.sdj.dumas@gmail.com>
2015-02-10 20:09:35 -0500

    Fully Implemented Finding P given a Geometric Gradient Cash Flow. All Tests Passed. Also Refactored

--- SSE659.TDDnDVCS/src/PgATest.java
+++ SSE659.TDDnDVCS/src/PgATest.java
@@ -3,23 +3,29 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class PgATest {
+	
+	EngEconomyCalc mycalc = new EngEconomyCalc();
+	double A, g, i, correct_answer;
+	int n;
 
 	@Test
 	public void test_g_eq_i() {//Testing the case where g equals i
-		EngEconomyCalc mycalc = new EngEconomyCalc();
-		double A = 2800000, g = 8.0, i = 8.0;
-		int n = 6;
-		double correct_answer = 15555555.56;//Found via calculator
+		
+		A = 2800000;
+		g = 8.0;
+		i = 8.0;
+		n = 6;
+		correct_answer = 15555555.56;//Found via calculator
 		assertEquals(mycalc.findPgGivenA(A,g,i,n),correct_answer,0.01);
 	}
 	
 	@Test
-	public void test_g_noteq_i(){
-		EngEconomyCalc mycalc = new EngEconomyCalc();
-		double A = 2800000, g = 8.0, i = 7.0;
-		int n = 6;
-		double correct_answer = 2876402.283;
+	public void test_g_noteq_i(){		
+		A = 2800000;
+		g = 8.0;
+		i = 7.0;
+		n = 6;
+		correct_answer = 2876402.283;
 		assertEquals(mycalc.findPgGivenA(A,g,i,n),correct_answer,0.01);
 	}
-
 }
